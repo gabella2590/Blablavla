d5af41feb40b05c8efd536469dd8a4dedf8ac264
gabella2590 <52690860+gabella2590@users.noreply.github.com>
2019-07-09 12:33:59 +0600

    added function to plus

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -19,7 +19,10 @@ namespace Calculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a + b;
+            textBox3.Text = c.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
