6c39f07083707fcdac5bcb98a8d9e5fa427b0260
gabella2590 <52690860+gabella2590@users.noreply.github.com>
2019-07-10 14:41:18 +0600

    Added two tests

---
+++ Calculator/Calculator.Tests/TwoArgumentsCalc/AdditionTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator.Tests.TwoArgumentsCalc
+{
+    [TestFixture]
+    public class AdditionTest
+    {
+        [Test]
+
+        public void AdditionCalculator()
+        {
+            ITwoArgumentsCalculator calculator = new AdditionCalculator();
+            double actual = calculator.Calculate(1, 1);
+            Assert.AreEqual(2,actual);
+
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/TwoArgumentsCalc/DivisionTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests.TwoArgumentsCalc
+{
+    [TestFixture]
+    public class DivisionTest
+    {
+        [Test]
+
+        public void DivisionCalculator()
+
+        {
+            ITwoArgumentsCalculator calculator = new DivisionCalculator();
+            double actual = calculator.Calculate(4, 2);
+            Assert.AreEqual(2, actual);
+
+        }
+    }
+}
